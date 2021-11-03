d72ce0686ef514933689325c4c7e135142ea9920
whaberer <91747264+willhaberer@users.noreply.github.com>
2021-11-03 19:31:24 -0400

    added base prompt structure

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
-const questions = [];
-
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "Please enter the name of the creator",
+      name: "name",
+    },
+    {
+      type: "input",
+      message: "What is the name of the project?",
+      name: "projectName",
+    },
+    {
+      type: "input",
+      message: "Please describe the project",
+      name: "description",
+    },
+    {
+      type: "input",
+      message: "Why was this project built?",
+      name: "reason",
+    },
+  ])
+  .then((response) => {
+    console.log(response);
+  });
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
