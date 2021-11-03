7f57970a3b31e280558c9da6c71f5597fdee75f3
whaberer <91747264+willhaberer@users.noreply.github.com>
2021-11-03 19:54:16 -0400

    added callback funciton in case of err

--- index.js
+++ index.js
@@ -68,7 +68,8 @@ inquirer
       ## License
       The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
       ---
-      `
+      `,
+      (err) => (err ? console.error(err) : console.log("README logged!"))
     );
   });
 // TODO: Create a function to write README file
